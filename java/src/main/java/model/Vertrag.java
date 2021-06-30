866105510dff9df998dbcda3b42ee90243625d98
JanPhillip20 <jan.phillip.me@outlook.de>
2021-06-30 10:47:13 +0200

    fixed build error

--- java/src/main/java/model/Vertrag.java
+++ java/src/main/java/model/Vertrag.java
@@ -4,13 +4,12 @@ import model.dataTypes.Datum;
 
 public class Vertrag {
 
-    // Vertrag muss in Package Model??
-
     double kosten;
     String beschreibung;
     String bezeichnung;
     Datum datum;
     int vertragsnummer;
-    //Kunde kunde;        // fehler im KLassendiagramm!!
-                        // Entweder Kunde im model.Vertrag oder model.Vertrag in Kunde beides geht aber nicht.
+
+
+
 }
